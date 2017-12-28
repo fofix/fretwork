28b30be16d716af9eb2e4b6aaa3d55eaa1f7d588
François Magimel <magimel.francois@gmail.com>
2017-12-28 16:37:16 +0100

    Replace str by bytes in the midi module

--- fretwork/midi/MidiFileParser.py
+++ fretwork/midi/MidiFileParser.py
@@ -39,7 +39,7 @@ class MidiFileParser(object):
         header_chunk_zise = raw_in.readBew(4)
 
         # check if it is a proper midi file
-        if header_chunk_type != 'MThd':
+        if header_chunk_type != b'MThd':
             raise TypeError("It is not a valid midi file!")
 
         # Header values are at fixed locations, so no reason to be clever
