1b5a28582137b42a0a5229c0daf5f055b543a146
François Magimel <magimel.francois@gmail.com>
2018-10-06 18:49:19 +0200

    Logs: make the log level customizable
    This will be useful to switch between multiple levels, for example for
    debugging.
    
    Fix #40

--- fretwork/log.py
+++ fretwork/log.py
@@ -4,7 +4,7 @@
 #####################################################################
 # Fretwork                                                          #
 # Copyright (C) 2006 Sami Kyöstilä                                  #
-#               2009-2017 FoFiX Team                                #
+#               2009-2018 FoFiX Team                                #
 #                                                                   #
 # This program is free software; you can redistribute it and/or     #
 # modify it under the terms of the GNU General Public License       #
@@ -33,7 +33,7 @@ Configure the logger in your launcher::
     from fretwork import log
 
     # configure the logger
-    log.configure('file.log')
+    log.configure('file.log', logging.DEBUG)
 
 
 Import and use it::
@@ -47,11 +47,16 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 
-def configure(log_filename):
-    """Configure logging"""
+def configure(log_filename, log_level=logging.INFO):
+    """
+    Configure logging.
+
+    :param log_filename: name of the file where logs are saved.
+    :param log_level: level of logs (default: logging.INFO).
+    """
     # create the logger
     logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
+    logger.setLevel(log_level)
 
     # formatter
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -59,7 +64,7 @@ def configure(log_filename):
     handler_filename = log_filename
     handler_max = 1000000  # 1 Mo
     file_handler = RotatingFileHandler(handler_filename, 'a', handler_max, 1)
-    file_handler.setLevel(logging.INFO)
+    file_handler.setLevel(log_level)
 
     # set handlers
     file_handler.setFormatter(formatter)
